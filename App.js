52a7446de99f3bf70275e65f589c84a720ec08b9
Farkas Csaba <fcsabi789@gmail.com>
2023-05-01 23:00:45 +0200

    kész

--- App.js
+++ App.js
@@ -2,9 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  function getEmployees() {
+    let host = 'http://localhost:8000/api/';
+    let endpoint = 'bycicle';
+    let url = host + endpoint;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+      setEmps(result)
+    });
+  }
+  useEffect( () => {
+    getBycicle();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>gepida</Text>
+      <FlatList 
+      data={emps}
+      renderItem={ ({item}) => (
+        <Text>{item.name}</Text>
+      )}
+      />
       <StatusBar style="auto" />
     </View>
   );
